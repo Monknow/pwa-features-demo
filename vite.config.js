abbd6fe5d2709ac922db36d27f5d63c3ddf11520
Juan <juanrodriguezcode@gmail.com>
2024-06-07 10:10:23 -0400

    letsgoo

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,15 @@ import {defineConfig} from "vite";
 
 export default defineConfig({
 	base: "/pwa-features-demo/",
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				contactPickerApi: resolve(__dirname, "contact-picker-api/index.html"),
+				deviceOrientationApi: resolve(__dirname, "device-orientation-api/index.html"),
+				screenOrientationApi: resolve(__dirname, "screen-orientation-api/index.html"),
+				vibrationApi: resolve(__dirname, "vibration-api/index.html"),
+			},
+		},
+	},
 });
