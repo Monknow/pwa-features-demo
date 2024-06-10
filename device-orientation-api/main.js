80f37969c26075457c30d2ed211ce32cc773643d
Juan <juanrodriguezcode@gmail.com>
2024-06-10 10:04:28 -0400

    minor fixes

--- device-orientation-api/main.js
+++ device-orientation-api/main.js
@@ -1,16 +1,16 @@
 import "./../style.css";
 import "./cube.css";
 
-const alphaSpan = document.querySelector("#alpha-span");
-const betaSpan = document.querySelector("#beta-span");
-const gammaSpan = document.querySelector("#gamma-span");
+const currentAlpha = document.querySelector(".currentAlpha");
+const currentBeta = document.querySelector(".currentBeta");
+const currentGamma = document.querySelector(".currentGamma");
 
 const cube = document.querySelector(".cube");
 
 window.addEventListener("deviceorientation", (event) => {
-	alphaSpan.textContent = `${event.alpha.toFixed(2)}°`;
-	betaSpan.textContent = `${event.beta.toFixed(2)}°`;
-	gammaSpan.textContent = `${event.gamma.toFixed(2)}°`;
+	currentAlpha.textContent = event.alpha;
+	currentBeta.textContent = event.beta;
+	currentGamma.textContent = event.gamma;
 
 	cube.style.transform = `rotateX(${event.beta}deg) rotateY(${event.gamma}deg) rotateZ(${event.alpha}deg)`;
 });
